d44cf7ccdffbaa3b6b8e8e07bd5a69d32ceb1854
symonmuchemi <simonkenya846@gmail.com>
2024-07-01 18:09:32 +0300

    update: added script

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = document.getElementById('first_name');
+let lastName = document.getElementById('last_name');
+let email = document.getElementById('email');
+let query = document.getElementById('General-enquiry');
+let submit = document.getElementById('submit');
+
+// error texts
+let firstNameError = document.getElementById('first_name-error')
+let lastNameError = document.getElementById('last_name-error')
+let emailError = document.getElementById('email-error')
+let queryError = document.getElementById('query-error')
+let messageError = document.getElementById('message-error')
+let consentError = document.getElementById('consent-error')
